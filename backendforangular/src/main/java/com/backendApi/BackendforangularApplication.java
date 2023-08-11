fe678970ed95aabb06bef233ad4cfb8eb1da4896
KReddy143 <141957626+KReddy143@users.noreply.github.com>
2023-08-11 12:57:06 +0530

    Add files via upload

---
+++ backendforangular/src/main/java/com/backendApi/BackendforangularApplication.java
@@ -0,0 +1,13 @@
+package com.backendApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendforangularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendforangularApplication.class, args);
+	}
+
+}
---
+++ backendforangular/src/test/java/com/backendApi/BackendforangularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendforangularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
